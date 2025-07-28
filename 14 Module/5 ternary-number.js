fc236d06849e5d4e365cfcf867465463cbcbccc5
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-07-28 22:11:26 +0600

    5 ternary-number.js

--- 14 Module/5 ternary-number.js
+++ 14 Module/5 ternary-number.js
@@ -10,3 +10,33 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+
+
+//** 1. Using if-else **/ /
+let num1 = 10;
+let num2 = 20;
+
+let result;
+
+if ( num1 > num2 ) {
+    result = num1 * 2;
+}
+
+else {
+    result = num1 + num2;
+}
+
+console.log("Resut:", result); // ['Reslt:', 30]
+
+
+
+
+
+// **2. Using Ternary Operator **//
+
+let num01 = 50;
+let num02 = 10;
+
+let results = (num01 > num02) ? num01 * 2 : num01 + num02;
+
+console.log("Result:", results); // [ 'Result:', 100 ]

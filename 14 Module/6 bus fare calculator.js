ae9460345acaefb42bfa555a5b9e0ae47e675fd6
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-07-28 22:49:04 +0600

    6 bus fare calculator.js

--- 14 Module/6 bus fare calculator.js
+++ 14 Module/6 bus fare calculator.js
@@ -6,3 +6,21 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+
+let age = 25;
+let isStudent = false;
+
+let ticketFare;
+
+if (age < 10) {
+  ticketFare = 0;
+} else if (isStudent) {
+  ticketFare = 800 * 0.5; // 50% discount
+} else if (age >= 60) {
+  ticketFare = 800 * 0.85; // 15% discount
+} else {
+  ticketFare = 800; // regular fare
+}
+
+console.log("Ticket Fare:", ticketFare + " tk"); //[ 'Ticket Fare:', '800 tk' ]

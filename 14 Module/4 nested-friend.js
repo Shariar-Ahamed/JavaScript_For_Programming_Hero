2a0ef1bc4fae3256163de7b3869dadb9af68555a
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-07-28 21:56:52 +0600

    4 nested-friend

--- 14 Module/4 nested-friend.js
+++ 14 Module/4 nested-friend.js
@@ -12,3 +12,29 @@ use nested if-else-if-else
 */
 
 
+let yourScore = 85;
+let friendScore = 80;
+
+
+if (yourScore > 80 ) {
+    if (friendScore > 80) {
+        console.log("Go for a lunch");
+    }
+
+    else if (friendScore >= 60) {
+        console.log("Good luck next time"); // 'Good luck next time'
+    }
+
+    else if (friendScore >= 40) {
+        console.log("Keep your friend's message unseen");
+    }
+
+    else {
+        console.log("Block your friend");
+    }
+}
+
+else {
+    console.log("Go home and sleep and act sad");
+}
+

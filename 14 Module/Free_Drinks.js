05537ac53aa132fc1d1f3958b50ce2b67321ce1b
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-07-28 21:00:46 +0600

    BMI Calculae

---
+++ 14 Module/Free_Drinks.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+
+let burgerPrice = 450;
+
+if (burgerPrice > 500) {
+    console.log("free Coke");
+}  
+else {
+    console.log("Coke price: 30tk");
+    }

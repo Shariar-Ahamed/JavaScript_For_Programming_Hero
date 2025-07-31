2338fbff9cb60f4048778214b41486399c1c22cf
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-07-31 08:17:47 +0600

    New Commit

---
+++ 15 Module loop/Practice/for_loop.js
@@ -0,0 +1,34 @@
+
+// while loop 
+
+let num = 2;              // Loop variable
+while(num < 5) {         // loop condition
+    console.log(num);   // loop statement
+    num++;             // loop veriable change
+}
+
+
+// For Loop 
+
+// for (   ;   ;   ) {
+// }
+
+
+for (let num = 2; num < 5;  num++ ){
+    console.log(num);
+}
+
+
+
+for( let num = 0; num < 10; num = num+2){
+    console.log(num);
+}
+
+
+let num1 = 0;                   
+while(num1 < 10) {             
+    num1 = num1+2;            // loop stat
+    console.log(num1)
+    num1
+}
+    

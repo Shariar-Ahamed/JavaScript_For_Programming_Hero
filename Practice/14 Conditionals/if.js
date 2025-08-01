4a87c6642ab8b96a0a1ef627c5679a3b35b1828e
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 09:50:56 +0600

    If Else

---
+++ Practice/14 Conditionals/if.js
@@ -0,0 +1,39 @@
+/**
+ * --------STURCTURE-------
+ * if (condition){
+ * 
+ * }
+ * 
+ */
+
+//----------------------------------
+if (3 < 10){
+    console.log("i have a small value"); // 'i have a small value'
+}
+
+//----------------------------------
+var biryaniPrice = 500;
+
+if (biryaniPrice > 400){
+    console.log("Mama give me biryani") // 'Mama give me biryani'
+}
+
+//-----------------------------------
+/**
+ * if (condition) {
+*       execute code if condition above is true
+*   }
+* else {
+*       execute code if the codition is falsel
+*   }
+*/
+
+
+var weight = 40;
+
+if (weight >= 60){
+    console.log("I will carry it by muself");
+}
+else{
+    console.log("I will rent a rickshaw"); // 'I will rent a rickshaw'
+}

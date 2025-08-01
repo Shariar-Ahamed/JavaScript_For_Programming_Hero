971dbf2ef9188d1ff446085c3401c48aad084f02
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 06:20:53 +0600

    learn: practice basic JavaScript

---
+++ Practice/13 Introduction to JavaScript/first.js
@@ -0,0 +1,18 @@
+
+// 5 think to declare a variable
+
+var num = 5;
+console.log(num) // out put : 5
+
+
+var age; 
+console.log(age) // undefined
+
+
+
+// Update Number : 
+var number = 10;
+
+number = 12;
+
+console.log(number); //  out put: 12

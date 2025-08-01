2de73458bd3b0162d031db8810399c5fba696c3e
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 08:18:51 +0600

    Task2

---
+++ Problem Solving/13 Introduction to JavaScript/Task2.js
@@ -0,0 +1,9 @@
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+
+var totalMark = Mathematics + Biology + Chemistry + Physics + Bangla;
+
+console.log((totalMark/5).toFixed(2)); // '71.04'

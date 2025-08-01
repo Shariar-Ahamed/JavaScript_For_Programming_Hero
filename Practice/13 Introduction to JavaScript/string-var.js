9ebc2d98b447153234693a9cafffe295911990ae
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 06:31:57 +0600

    learn: practice basic JavaScript

---
+++ Practice/13 Introduction to JavaScript/string-var.js
@@ -0,0 +1,14 @@
+// number
+var num = 5;
+console.log(typeof num); // 'number'
+
+
+// string variavle 
+
+var country = "Bangladesh"
+console.log(typeof country); // 'string'
+
+// voolean
+var isRich = true;
+console.log(isRich); // true
+

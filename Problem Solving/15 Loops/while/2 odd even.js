780e7248f070616687c6d9fded9c483cd3e2f3dd
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 23:27:06 +0600

    2 odd even.js

---
+++ Problem Solving/15 Loops/while/2 odd even.js
@@ -0,0 +1,19 @@
+// Find all the odd numbers from 61 to 100.
+let num = 61;
+
+while (num <= 100){
+    if ( num % 2 !== 0 ){
+        console.log(num);   // 64, 63, ....
+    }
+    num++
+}
+
+
+// Find all the even numbers from 78 to 98.
+
+let number = 78;
+
+while (number <= 98){
+    console.log(number);    // 78, 80, ....
+    number = number + 2;
+}

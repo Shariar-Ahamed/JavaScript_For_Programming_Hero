f387572142f79d173a3989a44ebc11c630205221
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 07:11:24 +0600

    learn: practice basic JavaScript

---
+++ Practice/13 Introduction to JavaScript/number.js
@@ -0,0 +1,27 @@
+
+var orangePrice = 20;
+var chocolate = 50.5;
+var applePrice = "20";
+
+
+
+console.log(orangePrice + chocolate) // 70.5
+console.log(orangePrice + applePrice) // '2020'
+
+console.log(typeof orangePrice); // 'number'
+console.log(typeof applePrice); // 'string'
+
+
+//
+var num = parseInt ("20")
+console.log(typeof num) // 'number'
+
+
+var number = parseFloat ("20.5")
+console.log(typeof number) //
+
+
+var a = 10.4;
+var b = 15.5;
+var total = a + b;
+console.log(total.toFixed(2)) // '25.90'

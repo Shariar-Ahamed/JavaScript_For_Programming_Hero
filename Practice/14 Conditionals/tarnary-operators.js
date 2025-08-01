7d6a7e48dc5fe77895ab20f37a6691fd396cc233
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 20:56:19 +0600

    Tarnary-Operators

---
+++ Practice/14 Conditionals/tarnary-operators.js
@@ -0,0 +1,18 @@
+
+let num = 40;
+
+if (num <= 40){
+    console.log("Passed"); // Passed
+}
+
+else {
+    console.log("Faild");
+}
+
+
+
+
+let age = 20;
+
+let adult = (age <= 25) ? "I am adult" : "I am not adult";
+console.log(adult); // I am adult

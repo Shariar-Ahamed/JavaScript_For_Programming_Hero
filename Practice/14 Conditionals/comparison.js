f307d45df57cc3b85ec6eca7c2418b31da464428
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 09:11:53 +0600

    learn: practice basic JavaScript

---
+++ Practice/14 Conditionals/comparison.js
@@ -0,0 +1,29 @@
+console.log(5 > 10);              // false
+console.log(5 < 10);             // true
+console.log(5 == 10);           // false
+console.log(10 == 10);         // true
+console.log(15 >= 10);        // true
+console.log(5 >= 10);        // false
+console.log(5 <= 10);       // true
+console.log(10 <= 10);     // true
+console.log(100 <= 10);   // false
+
+
+// -------------------------------------
+
+
+var applePrice = 80;
+var orangePrice = 50;
+console.log(applePrice > orangePrice ) // true
+
+
+// ---------------------------------------
+
+console.log(10 == 10)            // true (Number Type)
+console.log(10 === '10')        // fase (String Type)
+
+// --------------------------------------
+
+console.log(10 != 10);      // false
+console.log(10 !== "10");  // true
+

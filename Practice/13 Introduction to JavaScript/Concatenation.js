f7c6c304e660871a24132c55e3153719a7b635cb
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-01 07:50:08 +0600

    Concaenatin

---
+++ Practice/13 Introduction to JavaScript/Concatenation.js
@@ -0,0 +1,26 @@
+// Concatenation
+let firstName = "Shariar";
+let lastName = "Ahamed";
+let nickName = "Ripn";
+
+let fullName = firstName + " " + lastName + " " + nickName;
+
+console.log(fullName); // Shariar Ahamed Ripn
+
+
+
+// String Concatenation
+let number1 = "10";  // 1 String All Number value are String
+let number2 =  20;   
+let number3 =  30;
+let number4 =  30;
+
+let result = number1 + number2 + number3 + number4;
+
+console.log(result); // '102030'
+
+
+console.log(20+"10") // '2010'
+console.log(20-"10") // '10'
+console.log(20*"10") // '200'
+console.log(20/"10") // '2'
---
+++ Practice/13 Introduction to JavaScript/Mathematical_Operation.js
@@ -0,0 +1,43 @@
+// Addition Operation
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice); // 30
+
+// short hand
+var price = 10;
+// price = price + 2
+price += 2;
+console.log(price) // 12
+
+
+
+// Subtraction Operation
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice - eggPrice;
+console.log(totalPrice); // 10
+
+
+
+// Multiplication Operation
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice * eggPrice;
+console.log(totalPrice); // 200
+
+
+
+// Division Operation
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice / eggPrice;
+console.log(totalPrice); // 2
+
+
+
+// Modulus Operation
+var onionPrice = 5;
+var eggPrice = 2;
+var totalPrice = onionPrice % eggPrice;
+console.log(totalPrice); // 1

6f4b206794658f782ef31119010e90409839a756
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-02 00:17:01 +0600

    multiplication table

--- Practice/15 Loops/Sum.js
+++ Practice/15 Loops/Sum.js
@@ -5,6 +5,6 @@ while(num < 10){
     console.log(num); //1 2 3 4 5 6 7 8 9
 
     sum = sum + num;
-    console.log('Sum:', sum) // Sum: 1 3 6 10 15 21 28 36 45
+    console.log(sum) // Sum: 1 3 6 10 15 21 28 36 45
     num++;
 }
---
+++ Problem Solving/15 Loops/while/4 multiplication table.js
@@ -0,0 +1,11 @@
+//As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+let num = 5;
+let i = 1;
+
+while (i <= 10){
+    console.log(num * i); // 5, 10, 15, 20, 25, 30 .... 50.
+    i++;
+}
+
+
+    // console.log(num + " x " + i + " = " + (num * i));

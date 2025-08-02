2d755ade120c06607e098cce531ac3831d95e898
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-02 16:12:15 +0600

    Array

---
+++ Practice/15.5 Array , Array Traversal/array.js
@@ -0,0 +1,8 @@
+let num = [1, 2, 3, 4, 5, ];
+
+console.log(num); // [1, 2, 3, 4, 5, ]
+console.log(num.length); // 5  {Enement no 5}
+console.log(num[3]); // 4      {Get element by index}
+
+num[1] = 555;
+console.log(num) // [1, 555, 3, 4, 5, ] {Chsnge value}

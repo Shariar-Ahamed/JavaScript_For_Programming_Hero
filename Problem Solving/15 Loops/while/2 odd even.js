23f62c7870af0982c12f7c8dce765126a6711c83
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-02 06:41:44 +0600

    Odd Number

--- Problem Solving/15 Loops/while/2 odd even.js
+++ Problem Solving/15 Loops/while/2 odd even.js
@@ -2,15 +2,12 @@
 let num = 61;
 
 while (num <= 100){
-    if ( num % 2 !== 0 ){
-        console.log(num);   // 64, 63, ....
-    }
-    num++
+    console.log(num);
+    num = num + 2;
 }
 
 
 // Find all the even numbers from 78 to 98.
-
 let number = 78;
 
 while (number <= 98){

7239354c09fdfc4630be2a2ffea03e1e72a59bde
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-02 07:22:17 +0600

    1 century.js

---
+++ Problem Solving/15 Loops/break/1 century.js
@@ -0,0 +1,14 @@
+/* 
+
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+
+*/
+
+
+for ( let y = 0; y <= 200; y++ ){
+                console.log(y);
+                if ( y === 100 ){
+                    console.log("Y is 100"); // Y is 100
+                    break; 
+                }
+}
